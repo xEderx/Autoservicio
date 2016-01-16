b329719ebb2dc9c26371587b5bdf374e819cd826
cprosales <cprosalesluis@hotmail.com>
2016-01-16 06:55:54 -0600

    MODIFICACION PRODUCTO

--- Autoservicio/frmModificarProducto.Designer.vb
+++ Autoservicio/frmModificarProducto.Designer.vb
@@ -22,8 +22,142 @@ Partial Class frmModificaRproducto
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmModificaRproducto))
+        Me.txtprecio = New System.Windows.Forms.TextBox()
+        Me.cmbtipo = New System.Windows.Forms.ComboBox()
+        Me.btnMGuardar = New System.Windows.Forms.Button()
+        Me.txtexi = New System.Windows.Forms.TextBox()
+        Me.txtnombre = New System.Windows.Forms.TextBox()
+        Me.txtCodigo = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtprecio
+        '
+        Me.txtprecio.Location = New System.Drawing.Point(125, 167)
+        Me.txtprecio.Name = "txtprecio"
+        Me.txtprecio.Size = New System.Drawing.Size(140, 20)
+        Me.txtprecio.TabIndex = 59
+        '
+        'cmbtipo
+        '
+        Me.cmbtipo.FormattingEnabled = True
+        Me.cmbtipo.Location = New System.Drawing.Point(125, 119)
+        Me.cmbtipo.Name = "cmbtipo"
+        Me.cmbtipo.Size = New System.Drawing.Size(140, 21)
+        Me.cmbtipo.TabIndex = 58
+        '
+        'btnMGuardar
+        '
+        Me.btnMGuardar.BackgroundImage = CType(resources.GetObject("btnMGuardar.BackgroundImage"), System.Drawing.Image)
+        Me.btnMGuardar.Location = New System.Drawing.Point(204, 193)
+        Me.btnMGuardar.Name = "btnMGuardar"
+        Me.btnMGuardar.Size = New System.Drawing.Size(52, 48)
+        Me.btnMGuardar.TabIndex = 57
+        Me.btnMGuardar.UseVisualStyleBackColor = True
+        '
+        'txtexi
+        '
+        Me.txtexi.Location = New System.Drawing.Point(125, 90)
+        Me.txtexi.Name = "txtexi"
+        Me.txtexi.Size = New System.Drawing.Size(140, 20)
+        Me.txtexi.TabIndex = 56
+        '
+        'txtnombre
+        '
+        Me.txtnombre.Location = New System.Drawing.Point(125, 55)
+        Me.txtnombre.Name = "txtnombre"
+        Me.txtnombre.Size = New System.Drawing.Size(140, 20)
+        Me.txtnombre.TabIndex = 55
+        '
+        'txtCodigo
+        '
+        Me.txtCodigo.Location = New System.Drawing.Point(125, 22)
+        Me.txtCodigo.Name = "txtCodigo"
+        Me.txtCodigo.Size = New System.Drawing.Size(140, 20)
+        Me.txtCodigo.TabIndex = 54
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(20, 160)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(82, 13)
+        Me.Label5.TabIndex = 53
+        Me.Label5.Text = "Precio de venta"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(20, 128)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(31, 13)
+        Me.Label4.TabIndex = 52
+        Me.Label4.Text = "Tipo:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(19, 90)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 13)
+        Me.Label3.TabIndex = 51
+        Me.Label3.Text = "Exitencia:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(20, 55)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(47, 13)
+        Me.Label2.TabIndex = 50
+        Me.Label2.Text = "Nombre:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(20, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(85, 13)
+        Me.Label1.TabIndex = 49
+        Me.Label1.Text = "Codigo de barra:"
+        '
+        'frmModificaRproducto
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.txtprecio)
+        Me.Controls.Add(Me.cmbtipo)
+        Me.Controls.Add(Me.btnMGuardar)
+        Me.Controls.Add(Me.txtexi)
+        Me.Controls.Add(Me.txtnombre)
+        Me.Controls.Add(Me.txtCodigo)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmModificaRproducto"
         Me.Text = "Form2"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents txtprecio As TextBox
+    Friend WithEvents cmbtipo As ComboBox
+    Friend WithEvents btnMGuardar As Button
+    Friend WithEvents txtexi As TextBox
+    Friend WithEvents txtnombre As TextBox
+    Friend WithEvents txtCodigo As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
 End Class
